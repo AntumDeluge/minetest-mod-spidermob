ecb59e1f9811842395a4a09edbf82f264db751fa
Jordan Irwin <antumdeluge@gmail.com>
2021-05-19 20:36:11 -0700

    Fix blood particle spawner

--- api.lua
+++ api.lua
@@ -702,20 +702,19 @@ function spidermob:register_mob(name, def)
 				p.y = p.y + self.blood_offset
 
 				minetest.add_particlespawner({
-					5, --blood_amount, --amount
-					0.25, --time
-					{x=p.x-0.2, y=p.y-0.2, z=p.z-0.2}, --minpos
-					{x=p.x+0.2, y=p.y+0.2, z=p.z+0.2}, --maxpos
-					{x=0, y=-2, z=0}, --minvel
-					{x=2, y=2, z=2}, --maxvel
-					{x=-4,y=-4,z=-4}, --minacc
-					{x=4,y=-4,z=4}, --maxacc
-					0.1, --minexptime
-					1, --maxexptime
-					0.5, --minsize
-					1, --maxsize
-					false, --collisiondetection
-					self.blood_texture --texture
+					amount = self.blood_amount,
+					time = 0.25,
+					minpos = {x=p.x-0.2, y=p.y-0.2, z=p.z-0.2},
+					maxpos = {x=p.x+0.2, y=p.y+0.2, z=p.z+0.2},
+					minvel = {x=0, y=-2, z=0},
+					maxvel = {x=2, y=2, z=2},
+					minacc = {x=-4,y=-4,z=-4},
+					maxacc = {x=4,y=-4,z=4},
+					minexptime = 0.1,
+					maxexptime = 1,
+					minsize = 0.5,
+					maxsize = 1,
+					texture = self.blood_texture,
 				})
 			end
 
